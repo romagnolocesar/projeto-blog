11504869169aa3dae761102f2dda8911a91c218a
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-11 13:45:01 -0300

    Aula 70 - Construindo Sistema de Login pt1

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,20 +1,64 @@
 import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import firebase from '../../firebase';
+
+import './login.css';
 
 class Login extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            email: '',
+            password: ''
         }
+
+        this.entrar = this.entrar.bind(this);
+        this.login = this.login.bind(this);
+
+
+    }
+    
+    entrar(e){
+        alert('entrou');
+        e.preventDefault();
+    }
+
+    login = async () => {
+        const {email, password} = this.state;
+        
+        try{
+            await firebase.login(email, password)
+            .catch( (error) => {
+                if(error.code === 'auth/user-not-found'){
+                    alert("Este usuário não existe");
+                }else{
+                    alert(`Código de Erro: ${error.code}`);
+                }
+                return null;
+            });
+        }catch(error){
+            alert(error.message);
+        }
+
     }
 
     render(){
         return(
             <div>
-                <h1>Tela Login</h1>
+                <form onSubmit={this.entrar} id="login">
+                    <label>Email:</label><br/>
+                    <input type="email" autoComplete="off" autoFocus value={this.state.email} 
+                        onChange={(e) => {this.setState({email: e.target.value})}} placeholder="nome@email.com" /><br/>
+                    <label>Senha: </label><br/>
+                    <input type="password" autoComplete="off" value={this.state.password}
+                        onChange={ (e) => {this.setState({password: e.target.value})}} placeholder="Sua Senha"/><br/>
+                    <button type="submit">Logar</button><br/>
+
+                    <Link to="/register">Ainda não possui uma conta?</Link>
+                </form>
             </div>
         );
     }
 }
 
-export default Login
+export default withRouter(Login);
