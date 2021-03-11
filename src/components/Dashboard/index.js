bd1d7e655354063b337df4e0b84ac24d12bac9a9
Cesar Romagnolo <cesar.romagnolo@hotmail.com>
2021-03-11 20:40:22 -0300

    Aula 73 - Criando Painel Administrativo

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,20 +1,56 @@
 import React, {Component} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import firebase from '../../firebase';
 
 class Dashboard extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            nome: localStorage.nome,
+            email: localStorage.email,
         }
+
+        this.logout = this.logout.bind(this);
+
+    }
+
+    async componentDidMount(){
+        if(!firebase.getCurrent()){
+            this.props.history.replace('/login');
+            return null;
+        }
+
+        firebase.getUserName((info) => {
+            let nomeUsuario = info.val().nome;
+            let emailUsuario = info.val().email;
+            localStorage.nome = nomeUsuario;
+            localStorage.email = emailUsuario;
+            this.setState({
+                nome: localStorage.nome,
+                email: localStorage.email
+            });
+        });
+        
+    }
+
+    logout(){
+        firebase.logOut((result) => {
+            console.log(result);
+        });
     }
 
     render(){
         return(
-            <div>
-                <h1>Tela Dashboard</h1>
+            <div id="dashboard">
+                <div className="user-info">
+                    <h1>Olá {this.state.nome}</h1>
+                    <Link to='/dashboard/new'>Novo Post</Link>
+                </div>
+                <p>Logado com: {this.state.email}</p>
+                <button onClick={() => this.logout()}>Deslogar</button>
             </div>
         );
     }
 }
 
-export default Dashboard
+export default  withRouter(Dashboard);
--- src/firebase.js
+++ src/firebase.js
@@ -26,6 +26,10 @@ class Firebase{
        return app.auth().signInWithEmailAndPassword(email, password);
    }
 
+    logOut(email, password){
+        return app.auth().signOut();
+    }
+
    async register(nome, email, password){
        await app.auth().createUserWithEmailAndPassword(email, password);
 
@@ -41,8 +45,20 @@ class Firebase{
     }
 
     getCurrent(){
-        return app.auth().currentUser && app.auth().currentUser.email;
+        return app.auth().currentUser && app.auth().currentUser.email
     }
+
+    async getUserName(callback){
+        if(!app.auth().currentUser){
+            return null;
+        }
+
+        const uid = app.auth().currentUser.uid;
+        await app.database().ref('usuarios').child(uid)
+        .once('value')
+        .then(callback);
+    }
+    
 }
 
 export default new Firebase();
